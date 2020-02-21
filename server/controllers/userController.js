36a45a5c669c740913139001bd8bb5f4e39b11c7
Rachel Yoo <yoo.rache@gmail.com>
2020-02-20 16:01:27 -0800

    completed google oauth verification and insertion to database

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,7 +5,6 @@ const client = new OAuth2Client(
   '382771863992-hu7olpe3sfiae910a1urf4orija474oj.apps.googleusercontent.com'
 );
 
-// find and validate user; check if user already exists in database
 userController.verifyUser = (req, res, next) => {
   async function verify() {
     const ticket = await client.verifyIdToken({
@@ -14,30 +13,38 @@ userController.verifyUser = (req, res, next) => {
         '382771863992-hu7olpe3sfiae910a1urf4orija474oj.apps.googleusercontent.com'
     });
     const payload = ticket.getPayload();
-    const userId = payload['sub'];
-    next();
+
+    const { name, email, sub } = payload;
+    const values = [name, email, sub];
+    const query1 = `SELECT EXISTS(SELECT 1 FROM "user" WHERE googleId = '${payload.sub}')`;
+
+    const query2 = `INSERT INTO "user" (user_name, email, googleId)
+                    VALUES ($1, $2, $3)
+                    RETURNING *`;
+
+    const query3 = `SELECT * from "user" where googleId = '${payload.sub}'`;
+    let exists;
+
+    db.query(query1)
+      .then(data => {
+        exists = data.rows[0].exists;
+        if (exists === false) {
+          db.query(query2)
+            .then(data => {
+              res.locals.users = data.rows;
+              next();
+            })
+            .catch(err => console.log(err));
+        } else {
+          db.query(query3).then(data => {
+            res.locals.users = data.rows;
+            next();
+          });
+        }
+      })
+      .catch(err => console.log(err));
   }
   verify().catch(console.error);
 };
-
-//add user
-userController.createUser = (req, res, next) => {
-  // const { username, email } ?
-  // const values = [username, email] ?
-  const query = `INSERT INTO user (user_name, email)
-                 VALUES ($1, $2)
-                 RETURNING *`;
-  db.query(query, values)
-    .then(data => {
-      res.locals.users = data.rows;
-      next();
-    })
-    .catch(err => {
-      console.log(err);
-      next(err);
-    });
-};
-
-//get user history
 
 module.exports = userController;
